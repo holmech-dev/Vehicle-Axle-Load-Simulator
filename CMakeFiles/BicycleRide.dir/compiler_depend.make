08e21e753285517d9285d4a82deec1bf7413f910
Chris Holmes <holmech.dev@gmail.com>
2025-10-05 19:06:38 +0100

    build file tidying

--- CMakeFiles/BicycleRide.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for BicycleRide.
-# This may be replaced when dependencies are built.
--- CMakeFiles/BicycleRide.dir/tyreModel.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/BicycleRide.dir/tyreModel.cpp.o: \
-  /Users/holmech/code/vehicle_models/slope/tyreModel.cpp \
-  /Users/holmech/code/vehicle_models/slope/tyreModel.hpp
